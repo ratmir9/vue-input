df950038ff0e63603385446a06808431fce28273
ratmir9 <ratmir05050@gmail.com>
2023-09-28 01:10:45 +0300

    initial project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import TheForm from "./components/TheForm.vue";
+
+
+const printFormData = (formData) => {
+  console.group("Form Data")
+  for (const key in formData){
+    console.log(`${key} : ${formData[key]}`)
+  }
+  console.groupEnd()
+}
+
+
+</script>
+
+<template>
+  <TheForm @sendFormData="printFormData"/>
+</template>
---
+++ src/components/TheForm.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+import {ref} from "vue";
+
+const input1 = ref("")
+const input2 = ref("")
+const input3 = ref("")
+const input4 = ref("")
+
+const emit = defineEmits(['sendFormData'])
+
+const sendFormData = () => {
+  const formData = {
+    inputValue1: input1.value,
+    inputValue2: input2.value,
+    inputValue3: input3.value,
+    inputValue4: input4.value,
+  }
+  // console.log(formData)
+  emit("sendFormData", formData)
+}
+
+</script>
+
+<template>
+  <h1>Form</h1>
+  <form @submit.prevent="sendFormData">
+    <p><input type="text" v-model="input1"></p>
+    <p><input type="text" v-model="input2"></p>
+    <p><input type="text" v-model="input3"></p>
+    <p><input type="text" v-model="input4"></p>
+    <button type="submit">Отправить</button>
+  </form>
+</template>
+
+<style scoped>
+
+</style>
